73af12edea40ced60fb07b9c7c4cf97fa6726b05
rlaxogn <kth040918@gmail.com>
2024-11-20 12:08:57 +0000

    chap9

---
+++ chap9/prob9/shell.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAXARG 20
+
+int main() {
+	char *args[MAXARG];
+	
+	 printf("[Shell]\n");
+	 
+
+	 
+
+
+
+
+	 return 0;
+}
