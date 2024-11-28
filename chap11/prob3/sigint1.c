d533aae9fe06425f3bc473b0de5d251b2cdfa874
rlaxogn <kth040918@gmail.com>
2024-11-28 01:46:13 +0000

    chap11

---
+++ chap11/prob3/sigint1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+void intHandler();
+
+int main() 
+{
+	signal(SIGINT, intHandler);
+	while(1)
+		pause();
+	printf("end\n");
+}
+
+void intHandler(int signo)
+{
+	printf("inturrpy signanl\n");
+	printf("signal number : %d\n", signo);
+	exit(0);
+}
